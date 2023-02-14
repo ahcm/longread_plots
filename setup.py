a5da51079b89b7510e38bc42bd8ef5a4f30be2ff
Andreas Hauser <andreas.github@icloud.com>
2023-02-14 10:02:39 +0100

    Avoid matplotlib 3.6.1 which breaks seaborn. See https://github.com/matplotlib/matplotlib/issues/24127 and https://github.com/matplotlib/matplotlib/issues/24316

--- setup.py
+++ setup.py
@@ -20,4 +20,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'seaborn',
+          'matplotlib!=3.6.1',
+          'pandas'
+    ],
 )
